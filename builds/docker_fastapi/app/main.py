ce4dcb5737f7c69bd0708db404adf9517971ed35
Ernie Pedapati <ernest.pedapati@cchmc.org>
2024-04-07 19:14:11 -0400

    Update logging configuration and fix import in main.py

--- builds/docker_fastapi/app/main.py
+++ builds/docker_fastapi/app/main.py
@@ -1,7 +1,8 @@
 import logging
 from fastapi import FastAPI
 import os
-#from portal_utils import load_config
+from signalfloweeg.portal import portal_utils
+# from portal_utils import load_config
 #from process_uploads import process_new_uploads
 
 app = FastAPI()
@@ -9,8 +10,10 @@ app = FastAPI()
 @app.get("/process-uploads")
 def process_uploads():
     logging.info("Processing new uploads...")
-    #config = load_config()
-    #UPLOAD_PATH = config['folder_paths']['uploads']
+    config = portal_utils.load_config()
+    logging.info(config)
+    UPLOAD_PATH = config['folder_paths']['uploads']
+    logging.info(f"UPLOAD_PATH: {UPLOAD_PATH}")
     #drop_all_tables()
     #process_new_uploads(upload_dir=UPLOAD_PATH)
     return {"message": "Upload processing completed."}
@@ -19,5 +22,5 @@ if __name__ == "__main__":
     import uvicorn
     if not os.path.exists("portal_files/logs/"):
         os.makedirs("portal_files/logs/")
-    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", handlers=[logging.FileHandler("portal_files/logs/eeg_file_catalog.log"), logging.StreamHandler()])
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s", datefmt="%Y-%m-%d %H:%M:%S", handlers=[logging.FileHandler("portal_files/logs/sf_portal.log"), logging.StreamHandler()])
+    uvicorn.run(app, host="0.0.0.0", port=8001)
