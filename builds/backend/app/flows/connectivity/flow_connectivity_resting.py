dd065bef4c98821feebdcace9243e0cfe307aa2b
Nathan Suer <natesuer@gmail.com>
2024-07-08 15:34:25 -0400

    Deleting Un-needed files

--- builds/backend/app/flows/connectivity/flow_connectivity_resting.py
+++
@@ -1,7 +0,0 @@
-from fastapi import FastAPI, Response
-from fastapi.testclient import TestClient
-app = FastAPI()
-
-client = TestClient(app)
-response = client.get("/api/test/")
-print(response.json())
--- builds/build_docker.sh
+++
@@ -1,4 +0,0 @@
- docker build -t cbl/sf_backend -f docker_backend/dockerfile docker_backend/
- #docker build -t cbl/sf_portal -f docker_portal/dockerfile docker_portal/
- # docker build -t cbl/sf_octave -f docker_octave/dockerfile docker_octave/
- # docker build -t cbl/sf_airflow -f docker_airflow/dockerfile docker_airflow/
