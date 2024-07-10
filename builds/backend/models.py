65a459e286016c93675a6a1432b231a71f3f1c28
Nathan Suer <natesuer@gmail.com>
2024-07-10 09:12:12 -0400

    Update models.py
    Updated Database models

--- builds/backend/models.py
+++ builds/backend/models.py
@@ -19,13 +19,46 @@ class Study(BaseModel):
     start_date: datetime
     end_date: datetime
     description: str
+    
+class EEGFormat(BaseModel):
+    name: str
+    description: str
+    
+class Paradigms(BaseModel):
+    name: str
+    description: str
+    Study: Study
 
+class OriginalImportFile(BaseModel):
+    upload_id: str | None
+    original_name: str | None
+    is_set_file: bool | None
+    has_fdt_file: bool | None
+    fdt_filename: str | None
+    fdt_upload_id: str | None
+    dataset_id: str | None
+    dataset_name: str | None
+    eeg_format: EEGFormat | None
+    eeg_paradigm: Paradigms | None
+    paradigm_start_time: datetime
+    paradigm_duration: int
+    upload_email: str | None
+    status: str | None
+    date_added: str | None
+    hash: str | None
+    remove_import: bool | None
+    sample_rate: int | None
+    n_channels: int | None
+    n_epochs: int | None
+    total_samples: int | None
+    mne_load_error: bool | None
+    
 class File(BaseModel):
     status: str
     upload_id: str
     date_added: str
-    original_name: str
-    eeg_format: str
+    original_file: str
+    eeg_format: EEGFormat
     eeg_paradigm: str
     is_set_file: bool
     has_fdt_file: bool
@@ -34,20 +67,20 @@ class File(BaseModel):
     upload_email: str
     hash: str
     metadata: str
-
-class Paradigms(BaseModel):
-    ParadigmName: str
-    Study: Study
+    status: str
+    date_added: str | None = None
     
-class ParadigmTrial(BaseModel):
-    paradigm: Paradigms
-    start_time: datetime
-    duration: int
-    file: File
+class Dataset(BaseModel):
+    name: str
+    description: str
+    files: list[File]
+    status: str
+    date_added: str
+    date_modified: str
 
 class Session(BaseModel):
     participant_id: Participant
-    paradigm_trials: list[ParadigmTrial]
+    paradigm_trials: list[File]
     date: str
     equipment_used: str
     notes: str
@@ -69,92 +102,16 @@ class EegAnalysis(BaseModel):
     files: list[FileStatus]
     parameters: str
     
+class User(BaseModel):
+    username: str
+    email: str
+    password: str
+    is_active: bool
+    is_superuser: bool
     
-
-
-
-
-
-
-
-
-# EegAnalysis {
-# 	name: string
-# 	function_name string
-# 	description: string
-# 	category: string
-# 	valid_formats: string
-# 	valid_paradigms: string *>* Paradigms.id
-# 	valid_FIles integer *>* File.id
-# 	files integer *>* FileStatus.id
-# 	parameters: string
-# }
-
-
-# FileStatus {
-# 	id integer pk increments unique
-# 	file_id integer > File.id
-# 	current_status string
-# }
-
-# # Define the collections 
-# class EEGFormat(BaseModel):
-#     id: str
-#     name: str
-#     description: str
-
-# class EEGParadigm(BaseModel):
-#     id: str
-#     name: str
-#     description: str
-
-# class Email(BaseModel):
-#     id: str
-#     name: str
-
-# # Files Tab
-# class UploadCatalog(BaseModel):
-#     upload_id: str | None = None
-#     original_name: str | None = None
-#     is_set_file: bool | None = None
-#     has_fdt_file: bool | None = None
-#     fdt_filename: str | None = None
-#     fdt_upload_id: str | None = None
-#     dataset_id: str | None = None
-#     dataset_name: str | None = None
-#     eeg_format: str | None = None
-#     eeg_paradigm: str | None = None
-#     upload_email: str | None = None
-#     status: str | None = None
-#     date_added: str | None = None
-#     hash: str | None = None
-#     size: int | None = None
-#     remove_upload: bool | None = None
-
-# class ImportCatalog(BaseModel):
-#     upload_id: str | None
-#     original_name: str | None
-#     is_set_file: bool | None
-#     has_fdt_file: bool | None
-#     fdt_filename: str | None
-#     fdt_upload_id: str | None
-#     dataset_id: str | None
-#     dataset_name: str | None
-#     eeg_format: str | None
-#     eeg_paradigm: str | None
-#     upload_email: str | None
-#     status: str | None
-#     date_added: str | None
-#     hash: str | None
-#     remove_import: bool | None
-#     sample_rate: int | None
-#     n_channels: int | None
-#     n_epochs: int | None
-#     total_samples: int | None
-#     mne_load_error: bool | None
-
-# class DatasetCatalog(BaseModel):
-#     dataset_name: str | None
-#     dataset_id: str | None
-#     description: str | None
+class UserGroup(BaseModel):
+    name: str
+    description: str
+    users: list[User]
+    
 
