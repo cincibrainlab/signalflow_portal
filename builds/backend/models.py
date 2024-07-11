306e6fde70a03fa391feb10bba66bfdae81f1848
Nathan Suer <natesuer@gmail.com>
2024-07-11 09:43:01 -0400

    Update models.py

--- builds/backend/models.py
+++ builds/backend/models.py
@@ -3,38 +3,38 @@ from datetime import datetime
 
 # Define the collections
 class Participant(BaseModel):
-    participant_id: str
-    species: str
-    age: int
-    age_group: str
-    gender: str
-    handedness: str
-    diagnosis: str
-    iq_score: int
-    anxiety_level: int
+    participant_id: str | None
+    species: str | None
+    age: int | None
+    age_group: str | None
+    gender: str | None
+    handedness: str | None
+    diagnosis: str | None
+    iq_score: int | None
+    anxiety_level: int | None
     
 class Study(BaseModel):
-    study_name: str
-    principal_investigator: str
-    start_date: datetime
-    end_date: datetime
-    description: str
+    study_name: str | None
+    principal_investigator: str | None
+    start_date: datetime | None
+    end_date: datetime | None
+    description: str | None
     
 class EEGFormat(BaseModel):
-    name: str
-    description: str
+    name: str | None
+    description: str | None
     
 class EEGParadigm(BaseModel):
-    name: str
-    description: str
-    Study: Study
+    name: str | None
+    description: str | None
+    Study: Study | None
     
 class User(BaseModel):
-    username: str
-    email: str
-    password: str
-    is_active: bool
-    is_superuser: bool
+    username: str | None
+    email: str | None
+    password: str | None
+    is_active: bool | None
+    is_superuser: bool | None
 
 class OriginalImportFile(BaseModel):
     upload_id: str | None
@@ -46,8 +46,8 @@ class OriginalImportFile(BaseModel):
     dataset_id: str | None
     eeg_format: EEGFormat | None
     eeg_paradigm: EEGParadigm | None
-    paradigm_start_time: datetime
-    paradigm_duration: int
+    paradigm_start_time: datetime | None
+    paradigm_duration: int | None
     upload_user: User | None
     status: str | None
     date_added: str | None
@@ -58,66 +58,67 @@ class OriginalImportFile(BaseModel):
     n_epochs: int | None
     total_samples: int | None
     mne_load_error: bool | None
+    participant: Participant | None
     
 class File(BaseModel):
-    status: str
-    upload_id: str
-    date_added: str
-    original_file: OriginalImportFile
-    eeg_format: EEGFormat
-    is_set_file: bool
-    has_fdt_file: bool
-    fdt_filename: str
-    fdt_upload_id: str
-    hash: str
-    metadata: str
-    status: str
+    status: str | None
+    upload_id: str | None
+    date_added: str | None
+    original_file: OriginalImportFile | None
+    eeg_format: EEGFormat | None
+    is_set_file: bool | None
+    has_fdt_file: bool | None
+    fdt_filename: str | None
+    fdt_upload_id: str | None
+    hash: str | None
+    metadata: str | None
+    status: str | None
     
 class Dataset(BaseModel):
-    dataset_name: str
-    dataset_id:str
-    description: str
-    files: list[File]
-    status: str
-    date_added: str
-    date_modified: str
+    dataset_name: str | None
+    dataset_id:str | None
+    description: str | None
+    files: list[File] | None
+    status: str | None
+    date_added: str | None
+    date_modified: str | None
 
 class Session(BaseModel):
-    participant_id: Participant
-    paradigm_trials: list[File]
-    date: str
-    equipment_used: str
-    notes: str
-    recording_type: str
-    data_type: str
+    participant_id: Participant | None
+    paradigm_trials: list[File] | None
+    date: str | None
+    equipment_used: str | None
+    notes: str | None
+    recording_type: str | None
+    data_type: str | None
     
 class FileStatus(BaseModel):
-    file_id: File
-    current_status: str
+    file_id: File | None
+    current_status: str | None
     
 class EegAnalysis(BaseModel):
-    name: str
-    function_name: str
-    description: str
-    category: str
-    valid_formats: list[str]
-    valid_paradigms: list[EEGParadigm]
-    valid_files: list[File]
-    files: list[FileStatus]
-    parameters: str
+    name: str | None
+    function_name: str | None
+    description: str | None
+    category: str | None
+    valid_formats: list[str] | None
+    valid_paradigms: list[EEGParadigm] | None
+    valid_files: list[File] | None
+    files: list[FileStatus] | None
+    parameters: str | None
     
 class UserGroup(BaseModel):
-    name: str
-    description: str
-    users: list[User]
-    paradigms: list[EEGParadigm]
-    studies: list[Study]
-    datasets: list[Dataset]
-    participants: list[Participant]
-    files: list[File]
-    originalFiles: list[OriginalImportFile]
-    eegAnalyses: list[EegAnalysis]
-    sessions: list[Session]
+    name: str | None
+    description: str | None
+    users: list[User] | None
+    paradigms: list[EEGParadigm] | None
+    studies: list[Study] | None
+    datasets: list[Dataset] | None
+    participants: list[Participant] | None
+    files: list[File] | None
+    originalFiles: list[OriginalImportFile] | None
+    eegAnalyses: list[EegAnalysis] | None
+    sessions: list[Session] | None
     
     
 
